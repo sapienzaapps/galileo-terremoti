d59f96e2c37c7e2daf36047401f943757e18788a
Enrico204 <enrico204@gmail.com>
2015-10-09 23:51:39 +0200

    Added 10-seconds timeout to TCP functions

--- net/Tcp.cpp
+++ net/Tcp.cpp
@@ -3,10 +3,12 @@
 //
 
 #include "Tcp.h"
+#include "../Log.h"
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/fcntl.h>
 
 Tcp::Tcp() {
 	fd = 0;
@@ -67,6 +69,13 @@ bool Tcp::connectTo(IPaddr ipaddr, unsigned short port) {
 	addr.sin_port = htons(port);
 	addr.sin_addr.s_addr = htonl(ipaddr);
 
+	fd_set set;
+	FD_ZERO(&set);
+	FD_SET(fd, &set);
+
+	int flags = fcntl(fd, F_GETFL, 0);
+	fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+
 	int c = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
 	if(c < 0) {
 		// TODO: error
@@ -74,6 +83,28 @@ bool Tcp::connectTo(IPaddr ipaddr, unsigned short port) {
 		fd = 0;
 		return false;
 	}
+
+	struct timeval  timeout;
+	timeout.tv_sec = 10;
+	timeout.tv_usec = 0;
+
+	int status = select(fd+1, NULL, &set, NULL, &timeout);
+
+	if(status == -1) {
+		Log::e("Error during TCP select()");
+		fd = 0;
+		return false;
+	} else if(status == 0) {
+		Log::e("Timeout during connect to %s", ipaddr.asString().c_str());
+		close(fd);
+		shutdown(fd, SHUT_RDWR);
+		fd = 0;
+		return false;
+	}
+
+	// Restore blocking mode
+	fcntl(fd, F_SETFL, flags);
+
 	return true;
 }
 
@@ -84,6 +115,7 @@ ssize_t Tcp::readall(uint8_t *buf, size_t len) {
 
 void Tcp::stop() {
 	if(fd <= 0) return;
+	shutdown(fd, SHUT_RDWR);
 	close(fd);
 	fd = 0;
 }
