20dc881617af382f14e18bf76a0c4bb86afbb2ff
Enrico204 <enrico204@gmail.com>
2015-10-12 18:44:09 +0200

    Added missing pthread linking option

--- vendor/linux-x86/toolchain.mk
+++ vendor/linux-x86/toolchain.mk
@@ -3,5 +3,5 @@ CC         = gcc
 CPP        = g++
 CFLAGS     = ${MAINFLAGS}
 CPPFLAGS   = ${MAINFLAGS} -g -Wall -std=c++11 -Ivendor/${PLATFORM}/
-LFLAGS     = ${MAINFLAGS} -lm -lrt
+LFLAGS     = ${MAINFLAGS} -lm -lrt -lpthread
 STRIP      = strip
