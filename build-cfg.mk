f063eaa002c8c83af92fd381c36865aa5c7ca6ae
Enrico204 <enrico204@gmail.com>
2015-11-23 18:10:44 +0100

    Raspi auto-detection on Makefile

--- build-cfg.mk
+++ build-cfg.mk
@@ -2,10 +2,15 @@
 # Default platform is running OS
 
 UNAME_S := $(shell uname -s)
+LSB_RELEASE := $(shell lsb_release -i -s)
 
 ifeq ($(UNAME_S),Darwin)
 PLATFORM ?= mac-osx
 else
+ifeq ($(LSB_RELEASE),Raspbian)
+PLATFORM ?= raspi
+else
 PLATFORM ?= linux-x86
+endif
 endif
 
