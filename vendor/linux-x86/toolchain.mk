a8c8b7e30cef39a1c7b8259dfc9fe32196a4db74
Enrico204 <enrico204@gmail.com>
2015-12-04 00:58:24 +0100

    Added SDL debug output for demo

--- vendor/linux-x86/toolchain.mk
+++ vendor/linux-x86/toolchain.mk
@@ -8,5 +8,10 @@ CPP       ?= g++
 CFLAGS     = ${MAINFLAGS}
 CPPFLAGS   = ${MAINFLAGS} -g -Wall -std=c++11 -Ivendor/${PLATFORM}/
 LFLAGS     = ${MAINFLAGS} -lm -lrt -lpthread
+
+ifneq (, ${SDLDEMO})
+LFLAGS     = ${MAINFLAGS} -lm -lrt -lpthread -lSDL2
+endif
+
 STRIP      = strip
 
