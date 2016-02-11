1643e00b8377f35ea5c3ad6405a5a0ca6ec629d2
Enrico204 <enrico204@gmail.com>
2016-02-11 14:25:00 +0000

    Raspbian Wheezy support

--- build-cfg.mk
+++ build-cfg.mk
@@ -10,7 +10,7 @@ LSB_RELEASE := $(shell lsb_release -i -s)
 ifeq ($(LSB_RELEASE),Raspbian)
 PLATFORM ?= raspi
 else
-# Raspian Wheezy doesn't show Raspbian on LSB, so we're guessing from machine hardware
+# Raspian Wheezy doesn't show Raspbian on LSB, so we're guessing from machine hardware
 HWPLATFORM := $(shell uname -m)
 ifeq ($(HWPLATFORM),armv6l)
 PLATFORM ?= raspi
