244ffb198ea4524fd17dfadd563cc1e529cd8f64
Enrico204 <enrico204@gmail.com>
2016-02-11 15:44:35 +0100

    Support custom compilers on Raspberry PI target

--- vendor/raspi/toolchain.mk
+++ vendor/raspi/toolchain.mk
@@ -1,6 +1,6 @@
 
-CC         = gcc
-CPP        = g++
+CC        ?= gcc
+CPP       ?= g++
 CFLAGS     = ${MAINFLAGS}
 CPPFLAGS   = ${MAINFLAGS} -g -Wall -std=c++11 -Ivendor/${PLATFORM}/
 LFLAGS     = ${MAINFLAGS} -lm -lrt -lpthread -lwiringPi
